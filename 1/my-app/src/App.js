fb7b15a90a443b8ca795b2ab96993b83a93d1bd3
ashish5514 <zankatashish680@gmail.com>
2023-09-20 12:52:34 +0530

    changed

--- 1/my-app/src/App.js
+++ 1/my-app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform1";
+// import About from "./components/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <div className="container my-3">
         <Textform heading="Enter the text and changed Blow" />
+        {/* <About/> */}
       </div>
     </>
   );
